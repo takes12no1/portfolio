d6919c7b46fff008632e4fac1b15ef21689a3331
Mark Coleman <takes12no1@gmail.com>
2018-01-19 07:47:14 -0600

    build fixed

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp'),
-webpack = require('webpack');
+    webpack = require('webpack');
 
 gulp.task('scripts', function (callback) {
     webpack(require('../../webpack.config.js'), function (err, stats) {
         if (err) {
-           console.log(err.toString()); 
+            console.log("webpack error");
+            console.log(err.toString());
         }
         console.log(stats);
+        console.log("no webpack errors");
         callback();
     });
 });
-
